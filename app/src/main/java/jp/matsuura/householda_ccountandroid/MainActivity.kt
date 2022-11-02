faaa8bcfa4e7306807ea158db5f52aedbac63d06
matsuura1219 <matsuurayuki1219@gmail.com>
2022-11-02 20:54:14 +0900

    chore: 主要ライブラリの追加

--- app/src/main/java/jp/matsuura/householda_ccountandroid/MainActivity.kt
+++ app/src/main/java/jp/matsuura/householda_ccountandroid/MainActivity.kt
@@ -2,10 +2,30 @@ package jp.matsuura.householda_ccountandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Greeting("Android")
+        }
     }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        Greeting("Android")
+    }
+
 }
