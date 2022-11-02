ed406d3395b70f0965b4cd5e7a3bc4999727389c
matsuura1219 <matsuurayuki1219@gmail.com>
2022-11-02 21:44:52 +0900

    feat: スプラッシュ画面の実装

--- app/src/main/java/jp/matsuura/householda_ccountandroid/ui/MainActivity.kt
+++ app/src/main/java/jp/matsuura/householda_ccountandroid/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package jp.matsuura.householda_ccountandroid
+package jp.matsuura.householda_ccountandroid.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             Greeting("Android")
