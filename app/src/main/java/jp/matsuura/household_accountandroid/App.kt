c1ed2328d567cca46a9fedfef3d1acec7cde4102
matsuura1219 <matsuurayuki1219@gmail.com>
2022-11-03 12:42:09 +0900

    feat: Timberのセットアップ

--- app/src/main/java/jp/matsuura/household_accountandroid/App.kt
+++ app/src/main/java/jp/matsuura/household_accountandroid/App.kt
@@ -2,6 +2,31 @@ package jp.matsuura.household_accountandroid
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import jp.matsuura.householda_ccountandroid.BuildConfig
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @HiltAndroidApp
-class App: Application() {}
+class App: Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            initTimber()
+        }
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
