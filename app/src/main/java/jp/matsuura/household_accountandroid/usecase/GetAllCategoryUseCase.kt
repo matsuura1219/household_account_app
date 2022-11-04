b732357faee5720c820a755852f2b5d016a3660d
matsuura1219 <matsuurayuki1219@gmail.com>
2022-11-04 21:44:33 +0900

    feat: カテゴリ一覧を取得するUseCaseの実装

---
+++ app/src/main/java/jp/matsuura/household_accountandroid/usecase/GetAllCategoryUseCase.kt
@@ -0,0 +1,16 @@
+package jp.matsuura.household_accountandroid.usecase
+
+import jp.matsuura.household_accountandroid.data.repository.CategoryRepository
+import jp.matsuura.household_accountandroid.model.CategoryModel
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GetAllCategoryUseCase @Inject constructor(
+    private val categoryRepository: CategoryRepository,
+){
+
+    suspend operator fun invoke(): List<CategoryModel> {
+        return categoryRepository.getAll()
+    }
+}
